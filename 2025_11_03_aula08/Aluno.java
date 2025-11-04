12682beed47b5b6504e15fb497efcf6731efb4e5
SantosMelo123456 <lindamariasmelo@gmail.com>
2025-11-03 21:22:29 -0300

    exemplo

--- 2025_11_03_aula08/Aluno.java
+++ 2025_11_03_aula08/Aluno.java
@@ -2,6 +2,7 @@ public class Aluno extends Pessoa {
 
     private int matricula;
     private double nota1, nota2, nota3, media;
+    // comentario aleatorio
 
     public int getMatricula() {
         return matricula;
